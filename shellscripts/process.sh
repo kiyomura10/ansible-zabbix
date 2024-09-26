90d08f9b443b60a9e423443204eebb6bbc20e7ca
kiyomura10 <taiti199@yahoo.co.jp>
2024-09-27 00:08:52 +0900

    Update process.sh

--- shellscripts/process.sh
+++ shellscripts/process.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+set -eu
 
-process_count=$(ps aux | grep '[n]ginx' | wc -l)
+process_count=$(pgrep -a nginx | wc -l)
 echo "nginx process": $process_count
 
-if [[ $process_count -eq 0 ]]; then
+if [[ $process_count = 0 ]]; then
     echo "not running nginx"
     sudo systemctl start nginx
-    process_count=$(ps aux | grep '[n]ginx' | wc -l)
-    if [[ $process_count -gt 0 ]]; then
-        echo "nginx is running"
-    fi
+    echo "nginx is running"
 else 
     echo "nginx is running"
 fi
