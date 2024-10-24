ee16bb77d7799562d39e646ca1c0809019b41a14
kiyomura10 <taiti199@yahoo.co.jp>
2024-10-24 20:32:22 +0900

    設定ファイルの修正、nginx.conf追加

--- multipass_wordpress/wordpress_db_backup.sh
+++ multipass_wordpress/wordpress_db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# パイプライン内で失敗したコマンドがあれば、それを捕捉
-set -o pipefail 
+#コマンドが失敗したらスクリプト停止、未定義の変数を参照するとエラー 
+set -eu 
 
 DATE=$(date +'%Y%m%d')
 DB_NAME="wordpress_db"
@@ -9,11 +9,8 @@ DUMP_OPTIONS="--single-transaction --default-character-set=utf8mb4"
 BACKUP_FILE="/var/backups/mysql/wordpress_db_backup_$DATE.gz"
 
 #wordpress_dbバックアップの取得
-if mysqldump --login-path=backup $DUMP_OPTIONS $DB_NAME | gzip > $BACKUP_FILE; then
-	echo "Backup completed: $BACKUP_FILE";
+mysqldump --login-path=backup $DUMP_OPTIONS $DB_NAME | gzip > $BACKUP_FILE;
+echo "Backup completed: $BACKUP_FILE";
 	
-	#5日より前に作成されたファイルの削除
-	find /var/backups/mysql/ -name "wordpress_db_backup_*.gz" -type f -mtime +5 -exec echo "Deleting {}" \; -exec rm {} \;
-else
-	echo "Backup failed";
-fi
+#5日より前に作成されたファイルの削除
+rm /var/backups/mysql/wordpress_db_backup_$(date -d "5 days ago" +"%Y%m%d").gz
