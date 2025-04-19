47071e544a30b699eacf7c9d9b66a7fea902fadc
kiyomura10 <taiti199@yahoo.co.jp>
2025-04-19 18:01:20 +0900

    cronの修正

--- ansible-zabbix-grafana/roles/cron/templates/db_backup.sh
+++ ansible-zabbix-grafana/roles/cron/templates/db_backup.sh
@@ -4,13 +4,17 @@
 set -eu 
 
 DATE=$(date +'%Y%m%d')
-DB_NAME="wordpress_db"
+DB_NAME="{{ DB_NAME }}"
 DUMP_OPTIONS="--single-transaction --default-character-set=utf8mb4"
-BACKUP_FILE="/var/backups/mysql/wordpress_db_backup_$DATE.gz"
+BACKUP_DIR="/var/backups/mysql"
+BACKUP_FILE="/var/backups/mysql/${DB_NAME}${DATE}.gz"
 
-#wordpress_dbバックアップの取得
+# バックアップディレクトリがなければ作成
+mkdir -p "$BACKUP_DIR"
+
+#{{ DB_NAME }}バックアップの取得
 mysqldump --login-path=backup $DUMP_OPTIONS $DB_NAME | gzip > $BACKUP_FILE;
 echo "Backup completed: $BACKUP_FILE";
 	
 #5日より前に作成されたファイルの削除
-rm /var/backups/mysql/wordpress_db_backup_$(date -d "5 days ago" +"%Y%m%d").gz
+rm /var/backups/mysql/${DB_NAME}_$(date -d "5 days ago" +"%Y%m%d").gz
